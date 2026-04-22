d512edc14c57e3665fc529941fa91bdcf1b673a8
blanarlaci <blanarlaci@gmail.com>
2026-04-22 13:12:52 +0200

    kicsi frissitgetések

--- script.js
+++ script.js
@@ -9,3 +9,22 @@ function szamolas() {
         document.getElementById('eredmeny').innerText = "Kérlek, valós évet adj meg!";
     }
 }
+// Automatikus üdvözlés napszaknak megfelelően
+const ora = new Date().getHours();
+let uzenet = ora < 12 ? "Jó reggelt!" : ora < 18 ? "Jó napot!" : "Jó estét!";
+document.getElementById('udvozles').innerText = uzenet;
+
+// Sötét mód kapcsoló
+function darkMode() {
+    document.body.classList.toggle("dark-mode");
+}
+function berSzamolas() {
+    const brutto = document.getElementById('bruttoBer').value;
+    if (brutto > 0) {
+        const netto = brutto * 0.665; // 33.5% levonás után
+        document.getElementById('berEredmeny').innerText = 
+            "Nettó bér: " + Math.round(netto).toLocaleString() + " Ft";
+    } else {
+        alert("Kérlek adj meg egy összeget!");
+    }
+}
